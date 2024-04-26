3c0d8b37c8771305f5c5b9d2f9c882a07d32a7b1
yibotongxue <linyibo@stu.pku.edu.cn>
2024-04-26 14:53:16 +0800

    Create book.h and fix it

---
+++ book.h
@@ -0,0 +1,76 @@
+#pragma once
+#ifndef BOOK_H
+#define BOOK_H
+
+#include "citation.h"
+#include <vector>
+
+/**
+ * @brief Book class represent a book citation.
+ * 
+ * This class extends the functionality of the Citation class to represent
+ * a citation for a book. It inherits the unique identifier from the Citation
+ * class and may contain additional attributes specific to books, such as author(s),
+ * title, publisher, and publication year.
+*/
+class Book : public Citation {
+private:
+    std::vector<std::string> authors;
+    std::string title;
+    std::string publisher;
+    std::string year;
+
+public:
+    /**
+     * @brief Delete default constructor for Book class.
+     * 
+     * This default constructor is deleted to prevent accidental creation
+     * of Book objects without specifying required attributes such as author(s),
+     * title, publisher, and publication year.
+     * 
+     * To create a Book object, use the constructor with appropriate parameters.
+    */
+    Book() = delete;
+    
+    /**
+     * @brief Construct a new Book object with the given attributes.
+     * 
+     * @param id The unique identifier for the book citation.
+     * @param authors Ther author(s) of the book.
+     * @param title The title of the book.
+     * @param publisher The publisher of the book.
+     * @param year The publication year of the book.
+    */
+    Book(const std::string& id, const std::vector<std::string>& authors, const std::string& title, const std::string& publisher, const std::string& year);
+
+    /**
+     * @brief Copy constructor for Book class.
+     * 
+     * This constructor creates a new Book object as a copy of another Book object.
+     * 
+     * @param another The Book object to be copied.
+    */
+    Book(const Book& another);
+
+    /**
+     * @brief Default virtual destructor for Citation class.
+     * 
+     * This virtual destructor allows derived classes to properly clean up resources
+     * when objects are destroyed. It is provided with a default implementation.
+    */
+    virtual ~Book() = default;
+
+    /**
+     * @brief Clone the content of another Citation object.
+     * 
+     * This virtual function creates and returns a copy of the current Book object,
+     * effectively avoiding the slicing problem. Derived classes should override this
+     * function to provide proper cloning behavior specific to their type.
+     * 
+     * @param another The Citation object to clone.
+     * @return A reference to the newly created Book object.
+    */
+    virtual Citation& Clone(const Citation& another) override;
+};
+
+#endif
