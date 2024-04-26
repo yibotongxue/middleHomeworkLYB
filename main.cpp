5dad219b07ed1c6d6bafa35f8a389dc725ef5df8
yibotongxue <linyibo@stu.pku.edu.cn>
2024-04-26 23:14:01 +0800

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,63 @@
 #include "utils.hpp"
 #include "citation.h"
 
+#include "book.h"
+#include "webpage.h"
+#include "article.h"
+
+#include "third_parties/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+inline bool check_string(const json& j, const std::string& s) {
+    return j.contains(s) && j[s].is_string();
+}
+
+inline bool check_int(const json& j, const std::string& s) {
+    return j.contains(s) && j[s].is_number();
+}
+
+bool createCitationsPointer(std::vector<Citation*>& citations, const json& j) {
+    if(!check_string(j, "type") || !check_string(j, "id"))
+        return false;
+    auto type = j["type"].get<std::string>();
+    auto id = j["id"].get<std::string>();
+    if(type == "book") {
+        if(!check_string(j, "isbn")) {
+            return false;
+        }
+        auto isbn = j["isbn"].get<std::string>();
+    } else if(type == "webpage") {
+        if(!check_string(j, "url")) {
+            return false;
+        }
+        auto url = j["url"].get<std::string>();
+    } else if(type == "article") {
+        if(!check_string(j, "title") || !check_string(j, "author") || !check_string(j, "journal"))
+            return false;
+        if(!check_int(j, "year") || !check_int(j, "volume") || !check_int(j, "issue"))
+            return false;
+    } else {
+        return false;
+    }
+}
+
+void createCitations(std::vector<Citation*>& citations, const json& j) {
+    if(createCitationsPointer(citations, j))
+        return;
+    for(auto& item : j.items()) {
+        if(item.value().is_array()) {
+            for(auto& element : item.value()) {
+                if(element.is_object()){
+                    createCitations(citations, element);
+                }
+            }
+        } else if(item.value().is_object()) {
+            createCitations(citations, item.value());
+        }
+    }
+}
+
 std::vector<Citation*> loadCitations(const std::string& filename) {
     // FIXME: load citations from file
 }
