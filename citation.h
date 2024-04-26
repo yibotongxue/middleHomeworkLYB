8c7c9c2f4595a7793a28c114a6954c22e62e8b15
yibotongxue <linyibo@stu.pku.edu.cn>
2024-04-26 13:53:29 +0800

    Fix citation.h

--- citation.h
+++ citation.h
@@ -5,6 +5,42 @@
 #include <string>
 
 class Citation {
+
+protected:
+    std::string id; 
+
+    // Default assignment operator for Citation class
+    Citation& operator=(const Citation& c) = default; 
+
+public:
+    Citation() = delete; // delete the default constructor of Citation
+
+    /**
+     * @brief Construct a new Citation object with the given string
+     * 
+     * @param id The id of the citation
+     */
+    Citation(const std::string id);
+
+    /**
+     * @brief Construct a new Citation object as a copy of another Citation
+     * @param other The Citation to copy
+    */
+    Citation(const Citation& other);
+
+    ~Citation() = default;
+
+    /**
+     * @brief Clone the content of another CItation object.
+     * 
+     * This function clones the content of another Citation object and assigns it to
+     * the current Citation object using the assignment operator, effectively avoiding
+     * the slicing problem.
+     * 
+     * @param another The Citation object to clone.
+     * @return A reference to the current Citation object
+    */
+    virtual Citation& Clone(const Citation& another);
 };
 
 #endif
