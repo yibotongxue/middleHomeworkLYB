48b8670d3a59ef164477be5b2281203089269f10
yibotongxue <linyibo@stu.pku.edu.cn>
2024-04-26 14:52:32 +0800

    Update citation.h

--- citation.h
+++ citation.h
@@ -28,7 +28,7 @@ public:
      * 
      * @param id The id of the citation
      */
-    Citation(const std::string id);
+    Citation(const std::string& id);
 
     /**
      * @brief Construct a new Citation object as a copy of another Citation
@@ -37,17 +37,24 @@ public:
     */
     Citation(const Citation& other);
 
-    ~Citation() = default;
+    /**
+     * @brief Default virtual destructor for Citation class.
+     * 
+     * This virtual destructor is declared to ensure proper destruction of derived
+     * objects through base class pointers. It allows derived classes to clean up
+     * their resources properly when objects are destroyed through a base class pointer.
+    */
+    virtual ~Citation() = default;
 
     /**
-     * @brief Clone the content of another CItation object.
+     * @brief Clone the content of another Citation object.
      * 
-     * This function clones the content of another Citation object and assigns it to
-     * the current Citation object using the assignment operator, effectively avoiding
-     * the slicing problem.
+     * This virtual function creates and returns a copy of the current Citation object,
+     * effectively avoiding the slicing problem. Derived classes should override this 
+     * function to provide proper cloning behavior specific to their type.
      * 
      * @param another The Citation object to clone.
-     * @return A reference to the current Citation object
+     * @return A reference to the current Citation object.
     */
     virtual Citation& Clone(const Citation& another);
 };
