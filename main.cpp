6c076b0316bb1997379b35a6c10d564a14f32931
yibotongxue <linyibo@stu.pku.edu.cn>
2024-04-27 11:11:59 +0800

    Reset main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,71 @@
 #include "utils.hpp"
 #include "citation.h"
 
+#include "book.h"
+#include "webpage.h"
+#include "article.h"
+
+#include "third_parties/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+inline bool check_string(const json& j, const std::string& s) {
+    return j.contains(s) && j[s].is_string();
+}
+
+inline bool check_int(const json& j, const std::string& s) {
+    return j.contains(s) && j[s].is_number();
+}
+
+bool createCitationsPointer(std::vector<Citation*>& citations, const json& j) {
+    if(!check_string(j, "type") || !check_string(j, "id"))
+        return false;
+    auto type = j["type"].get<std::string>();
+    auto id = j["id"].get<std::string>();
+    if(type == "book") {
+        if(!check_string(j, "isbn")) {
+            return false;
+        }
+        auto isbn = j["isbn"].get<std::string>();
+    } else if(type == "webpage") {
+        if(!check_string(j, "url")) {
+            return false;
+        }
+        auto url = j["url"].get<std::string>();
+    } else if(type == "article") {
+        if(!check_string(j, "title") || !check_string(j, "author") || !check_string(j, "journal"))
+            return false;
+        if(!check_int(j, "year") || !check_int(j, "volume") || !check_int(j, "issue"))
+            return false;
+        auto title = j["title"].get<std::string>();
+        auto author = j["title"].get<std::string>();
+        auto journal = j["journal"].get<std::string>();
+        int year = j["year"];
+        int volume = j["volume"];
+        int issue = j["issue"];
+        citations.push_back(dynamic_cast<Citation*>(new Article(id, title, author, journal, year, volume, issue)));
+    } else {
+        return false;
+    }
+    return true;
+}
+
+void createCitations(std::vector<Citation*>& citations, const json& j) {
+    if(createCitationsPointer(citations, j))
+        return;
+    for(auto& item : j.items()) {
+        if(item.value().is_array()) {
+            for(auto& element : item.value()) {
+                if(element.is_object()){
+                    createCitations(citations, element);
+                }
+            }
+        } else if(item.value().is_object()) {
+            createCitations(citations, item.value());
+        }
+    }
+}
+
 std::vector<Citation*> loadCitations(const std::string& filename) {
     // FIXME: load citations from file
 }
@@ -21,10 +86,9 @@ int main(int argc, char** argv) {
 
     std::ostream& output = std::cout;
 
-    // output << input;  // print the paragraph first
-    // output << "\nReferences:\n";
-    
     for (auto c : printedCitations) {
+        // output << input;  // print the paragraph first
+        output << "\nReferences: \n";
         // FIXME: print citation
     }
 
