252a11984e89ff724ad3762cd27986f93c30f3ef
yibotongxue <linyibo@stu.pku.edu.cn>
2024-04-27 23:50:07 +0800

    add about file to debug

--- main.cpp
+++ main.cpp
@@ -157,7 +157,7 @@ std::vector<Citation*> loadCitations(const std::string& filename) {
     std::ifstream file{ filename };
 
     if(!file.is_open()) {
-        std::cout << "打开文件失败:"  <<  filename << "\n";
+        std::cout << "文献合集打开文件失败:"  <<  filename << "\n";
         exit(1);
     }
 
@@ -185,7 +185,7 @@ std::string readFromFile(const std::string& filename) {
     std::ifstream file{ filename };
 
     if(!file.is_open()) {
-        std::cout << "文件打开失败:" << filename << "\n";
+        std::cout << "输入文件打开失败:" << filename << "\n";
         exit(1);
     }
 
